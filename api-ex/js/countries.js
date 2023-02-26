a8551df989488958ad36296016c46564d1975dc2
arefin_aareef <arefin.aareef@gmail.com>
2023-02-27 02:02:33 +0600

    api examples

---
+++ api-ex/js/countries.js
@@ -0,0 +1,46 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+}
+
+const displayCountries = countries => {
+    const countriesContainer = document.getElementById('all-countries');
+    // console.log(countries);
+    // for(const country of countries){
+    //     console.log(country);
+    // }
+    countries.forEach(country => {
+
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('country');
+        countryDiv.innerHTML = `
+            <h3>Name: ${country.name.common} </h3>
+            <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
+            <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
+        `
+
+        countriesContainer.appendChild(countryDiv)
+
+
+        // console.log(country.name.common);
+    });
+}
+
+const loadCountryDetails = code => {
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showCountryDetails(data[0]))
+    // console.log(url);
+}
+
+const showCountryDetails = country => {
+    const detailContainer = document.getElementById('country-detail')
+    detailContainer.innerHTML = `
+        <h3>Name: ${country.name.common}</h3>
+        <img src="${country.flags.png}">
+    `
+}
+
+loadCountries();
